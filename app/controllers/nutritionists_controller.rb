3dc1014acad947de7d3e7c52fc0c0bfd9ac501f7
Celine Te <celine.te.mcgill@gmail.com>
2018-08-22 14:32:20 -0400

    Nutritionist show needs pictures

--- app/controllers/nutritionists_controller.rb
+++ app/controllers/nutritionists_controller.rb
@@ -1,2 +1,36 @@
 class NutritionistsController < ApplicationController
+  before_action :set_nutritionist, only: [:show]
+  skip_before_action :authenticate_user!, only:[:index, :show]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @nutritionist = Nutritionist.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+# private
+
+#   def nutritionist_params
+#     prms = params.require(:nutritionist).permit(:name, :bio, :created_at, :updated_at, :photo, :review, :certificate, :subscription)
+#     prms[:user_id] = current_user.id
+#     prms[:created_at] = DateTime.now
+#     return prms
+#   end
+
+  def set_nutritionist
+    @nutritionist = Nutritionist.find(params[:id])
+  end
 end

16cb01a677626a41ffe1ab5e79a304a66d32e332
Kojo Addaquay <kojoaddaquay@gmail.com>
2018-08-24 14:25:49 -0400

    added meal suggestions from API

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,6 +3,7 @@ class MealsController < ApplicationController
     @meal = Meal.find(params[:id])
     # hit the api with this meal to get the recEdamamService(@meal).call
     @ingredients = @meal.ingredients
+    @suggestions = @meal.nutritionist.meals.where.not(id: @meal.id).limit(3)
   end
 
   private

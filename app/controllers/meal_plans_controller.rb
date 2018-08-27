77c6d1005b2c73dc3659d6b920bbd0d5209d0e5c
Nordin Belkacemi <belkaceminordin@gmail.com>
2018-08-27 13:06:24 -0400

    tabs backend done

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -3,4 +3,13 @@ class MealPlansController < ApplicationController
   def index
     @meal_plans = MealPlan.all
   end
+
+  def new
+    if params[:nutritionist_id].nil?
+      @nutritionist = Nutritionist.first
+    else
+      @nutritionist = Nutritionist.find(params[:nutritionist_id])
+      puts "nutritionist -> #{@nutritionist.name}"
+    end
+  end
 end
